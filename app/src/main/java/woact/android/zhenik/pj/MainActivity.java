bbd3fc793bbf4374d85616c5b2ad39c24b39786b
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-04-16 17:32:31 +0200

    update MainActivity -> expand navigation functionality

--- app/src/main/java/woact/android/zhenik/pj/MainActivity.java
+++ app/src/main/java/woact/android/zhenik/pj/MainActivity.java
@@ -3,13 +3,23 @@ package woact.android.zhenik.pj;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.text.Layout;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import woact.android.zhenik.pj.fragment.GroupFragment;
+import woact.android.zhenik.pj.fragment.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FrameLayout mContent;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +28,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+//                    mTextMessage.setText(R.string.title_home);
+                    initProfileFragment();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+//                    mTextMessage.setText(R.string.title_dashboard);
+                    initGroupFragment();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+//                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -36,10 +48,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        mContent = (FrameLayout) findViewById(R.id.content);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        initProfileFragment();
+    }
+
+    private void initProfileFragment(){
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+//        if (fm.findFragmentByTag(ProfileFragment.TAG)==null)
+            transaction.replace(R.id.content, new ProfileFragment(), ProfileFragment.TAG);
+        transaction.commit();
+    }
+
+    private void initGroupFragment(){
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+//        if (fm.findFragmentByTag(GroupFragment.TAG)==null)
+            transaction.replace(R.id.content, new GroupFragment(), GroupFragment.TAG);
+        transaction.commit();
     }
 
 }
