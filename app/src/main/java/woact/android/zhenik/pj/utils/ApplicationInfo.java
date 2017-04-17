860748fea5772d0ca945bfa3246252c064d1687a
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-04-17 15:40:59 +0200

    refactor app info

--- app/src/main/java/woact/android/zhenik/pj/utils/ApplicationInfo.java
+++ app/src/main/java/woact/android/zhenik/pj/utils/ApplicationInfo.java
@@ -1,8 +1,11 @@
 package woact.android.zhenik.pj.utils;
 
 import java.io.Serializable;
-
+/**
+ * CONTEXT GLOBAL VARIABLES (No session)
+ * */
 public class ApplicationInfo implements Serializable {
+
     public static final String USER ="USER";
     public static long USER_IN_SYSTEM_ID;
 }
