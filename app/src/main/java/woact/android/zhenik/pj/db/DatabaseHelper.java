95fb85b2837f372b5f989eef28896e191e96d6cb
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-04-17 15:41:17 +0200

    refactor db helper

--- app/src/main/java/woact/android/zhenik/pj/db/DatabaseHelper.java
+++ app/src/main/java/woact/android/zhenik/pj/db/DatabaseHelper.java
@@ -90,70 +90,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
 
-
-//    // Adding new User
-//    public long addUser(User user) {
-////        checkDb();
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        ContentValues values = new ContentValues();
-//        values.put(KEY_USER_NAME, user.getUserName());
-//        values.put(KEY_PASSWORD, getHashWithSalt(user.getPassword()));
-//        values.put(KEY_FULL_NAME, user.getFullName());
-//
-//        // Inserting Row
-//        long status = db.insert(TABLE_USERS, null, values);
-//        db.close();
-////        Log.d(TAG, "userAdd status: " + status);
-//        return status;
-//    }
-//
-////    Get user by id
-//    public User getUser(long id) {
-//        SQLiteDatabase db = this.getReadableDatabase();
-//        Cursor cursor = db.query(
-//                TABLE_USERS,
-//                new String[] {KEY_ID, KEY_USER_NAME, KEY_PASSWORD, KEY_FULL_NAME},
-//                KEY_ID + "=?",
-//                new String[] { String.valueOf(id) },
-//                null, null, null, null);
-//        if (cursor != null)
-//            cursor.moveToFirst();
-//        else
-//            return null;
-//
-//        User user = new User(Long.parseLong(cursor.getString(0)),
-//                             cursor.getString(1),
-//                             cursor.getString(2),
-//                             cursor.getString(3));
-////        Log.d(TAG, user.toString());
-//        return user;
-//    }
-//
-//
-////     Getting All Users
-
-
-
-//
-//    // Updating single user
-//    /**
-//     * TODO: Need to update 1 field by 1.
-//     * Be careful with password, don't update all with 1 transaction
-//     * */
-//    private long updateUser(User user) {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//
-//        ContentValues values = new ContentValues();
-//        values.put(KEY_USER_NAME, user.getUserName());
-//        values.put(KEY_PASSWORD, user.getPassword());
-//        values.put(KEY_FULL_NAME, user.getFullName());
-//
-//        // updating row
-//        return db.update(TABLE_USERS, values, KEY_ID + " = ?",
-//                         new String[] { String.valueOf(user.getId()) });
-//    }
-//
-
 //
 //    // Deleting single contact
 //    public void deleteUser(User user) {
@@ -164,28 +100,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 ////        Log.d(TAG, rowsDeleted+"");
 //    }
 
-//
-//
-//
-//    private void printList(List<User> list){
-//        for (User u : list)Log.d(TAG,u.toString());
-//    }
-//
-//    public void clearDb(){
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        Log.d(TAG, "--- Clear " +TABLE_USERS+ " ---");
-//        // удаляем все записи
-//        int clearCount = db.delete(TABLE_USERS, null, null);
-//        Log.d(TAG, "deleted rows count = " + clearCount);
-//        db.close();
-//    }
-//
-//    private void checkDb(){
-//        String selectQuery = "SELECT  * FROM " + TABLE_USERS;
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        Cursor cursor = db.rawQuery(selectQuery, null);
-//        Log.d(TAG, Arrays.asList(cursor.getColumnNames()).toString());
-//    }
+
+
 
 
 
