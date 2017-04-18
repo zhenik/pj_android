5e76e5ce29c5dbbecbb1ea4279de3f33764cf65a
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-04-18 20:33:09 +0200

    update user group dao -> refactor & expand functionality

--- app/src/main/java/woact/android/zhenik/pj/db/UserGroupDao.java
+++ app/src/main/java/woact/android/zhenik/pj/db/UserGroupDao.java
@@ -8,11 +8,17 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
+import woact.android.zhenik.pj.model.Group;
 import woact.android.zhenik.pj.model.User;
 
+import static woact.android.zhenik.pj.db.DatabaseHelper.KEY_AVAILABLE_MONEY;
 import static woact.android.zhenik.pj.db.DatabaseHelper.KEY_GROUP_ID;
 import static woact.android.zhenik.pj.db.DatabaseHelper.KEY_ID;
+import static woact.android.zhenik.pj.db.DatabaseHelper.KEY_INVESTMENT;
+import static woact.android.zhenik.pj.db.DatabaseHelper.KEY_LOAN;
+import static woact.android.zhenik.pj.db.DatabaseHelper.KEY_TOTAL_MONEY;
 import static woact.android.zhenik.pj.db.DatabaseHelper.KEY_USER_ID;
+import static woact.android.zhenik.pj.db.DatabaseHelper.TABLE_GROUPS;
 import static woact.android.zhenik.pj.db.DatabaseHelper.TABLE_USERS;
 import static woact.android.zhenik.pj.db.DatabaseHelper.TABLE_USER_GROUP;
 
@@ -31,7 +37,9 @@ public class UserGroupDao {
         this(new UserDao(), new GroupDao());
     }
 
-    /** TODO: TEST IT
+
+
+    /**
      * @return userGroupId if operation successful, -1 if not
      * */
     public long registerUserInGroup(long userId, long groupId) {
@@ -104,28 +112,42 @@ public class UserGroupDao {
             } while (cursor.moveToNext());
         }
         return userList;
+    }
+
+    public List<Group> getGroupsOfUser(long userId){
+//        String selectQuery ="SELECT "+ TABLE_GROUPS +".*" + " FROM "+TABLE_GROUPS+
+//                " INNER JOIN "+TABLE_USER_GROUP+" ON "+TABLE_GROUPS+"."+KEY_ID+"="+TABLE_USER_GROUP+"."+KEY_GROUP_ID+
+//                " WHERE "+TABLE_USER_GROUP+"."+KEY_USER_ID+"=?";
+
+        String selectQuery = String.format(
+                "SELECT %s.* FROM %s INNER JOIN %s ON %s.%s=%s.%s WHERE %s.%s=?",
+                TABLE_GROUPS, TABLE_GROUPS, TABLE_USER_GROUP, TABLE_GROUPS, KEY_ID,
+                TABLE_USER_GROUP, KEY_GROUP_ID, TABLE_USER_GROUP, KEY_USER_ID);
+
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, new String[] {String.valueOf(userId)});
+        return getGroupsFromCursor(cursor);
 
     }
-//    // TODO: test it
-//    private List<Long> getUsersIdFromGroup(long groupId){
-//        List<Long> usersId = new ArrayList<Long>();
-//        // Select All Query
-//        String selectQuery = "SELECT " +KEY_USER_ID+ " FROM " + TABLE_USER_GROUP;
-//        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
-//        Cursor cursor = db.rawQuery(selectQuery, null);
-//
-//        if (cursor.moveToFirst()) {
-//            do {
-//                Long userId = cursor.getLong(0);
-//                usersId.add(userId);
-//            } while (cursor.moveToNext());
-//
-//            return usersId;
-//        }
-//        else
-//            return null;
-//
-//    }
+    private List<Group> getGroupsFromCursor(Cursor cursor){
+        List<Group> groupList = new ArrayList<>();
+        Group group;
+        if (cursor != null && cursor.getCount()>0)
+            cursor.moveToFirst();
+
+        if (cursor.moveToFirst()) {
+            do {
+                group = new Group();
+                group.setId(cursor.getLong(0));
+                group.setTotalMoney(cursor.getDouble(1));
+                group.setAvailableMoney(cursor.getDouble(2));
+                // Adding contact to list
+                groupList.add(group);
+
+            } while (cursor.moveToNext());
+        }
+        return groupList;
+    }
 
 
     /**
