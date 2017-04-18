27d5055aa794b7441b91d833552a2443db4da980
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-04-18 20:26:36 +0200

    update profile feature -> db interaction

--- app/src/main/java/woact/android/zhenik/pj/fragment/ProfileFragment.java
+++ app/src/main/java/woact/android/zhenik/pj/fragment/ProfileFragment.java
@@ -7,25 +7,30 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Switch;
 import android.widget.TextView;
 
+import com.mikhaellopez.circularimageview.CircularImageView;
+
 import woact.android.zhenik.pj.R;
+import woact.android.zhenik.pj.db.UserDao;
+import woact.android.zhenik.pj.db.UserGroupDao;
 import woact.android.zhenik.pj.model.User;
+import woact.android.zhenik.pj.utils.ApplicationInfo;
 
 
 public class ProfileFragment extends Fragment {
     public static final String TAG = "###ProfileFragment:> ";
 
-    private TextView name;
-    private TextView userTelephone;
-    private TextView userScore;
-    private TextView userName;
-    private TextView email;
-    private Switch isDnbCustomer;
+    private TextView mFullname;
+    private TextView mMoney;
+    private TextView mGroups;
+    private TextView mScore;
+    private CircularImageView mImg;
     private View view;
     private User user;
 
+    private UserDao userDao;
+    private UserGroupDao userGroupDao;
 
     public ProfileFragment() {
         Log.i(TAG, "constructor");
@@ -34,9 +39,41 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        btn = (Button) getActivity().findViewById(R.id.button2);
         Log.i(TAG, "onCreateView");
-        return this.view=inflater.inflate(R.layout.profile_fragment, null);
+        this.view=inflater.inflate(R.layout.profile_fragment, null);
+        initInfoElements();
+        initDaos();
+        return view;
     }
 
+    private void initDaos(){
+        userDao=new UserDao();
+        userGroupDao=new UserGroupDao();
+    }
+    private void initInfoElements(){
+        mFullname=(TextView)view.findViewById(R.id.profile_fullname_txt);
+        mMoney=(TextView)view.findViewById(R.id.profile_money_txt);
+        mGroups=(TextView)view.findViewById(R.id.profile_groups_txt);
+        mScore=(TextView)view.findViewById(R.id.profile_score_txt);
+        mImg=(CircularImageView)view.findViewById(R.id.profile_img_circularImageView);
+    }
+
+    private void updateInfo(){
+        user = userDao.getUserById(ApplicationInfo.USER_IN_SYSTEM_ID);
+        mFullname.setText(user.getFullName());
+        mMoney.setText(String.valueOf(user.getMoney()));
+        mScore.setText(String.valueOf(user.getScore()));
+        int amountGroups = userGroupDao.getGroupsOfUser(ApplicationInfo.USER_IN_SYSTEM_ID).size();
+        mGroups.setText(String.valueOf(amountGroups));
+
+        // set default pic TODO: development mode (hardcoded)
+        if ("a".equals(user.getUserName())) mImg.setImageResource(R.drawable.changiz);
+        if ("b".equals(user.getUserName())) mImg.setImageResource(R.drawable.oda);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateInfo();
+    }
 }
