42be1d7bacd28187d0903a56e8a78c43923c7d2d
Nikita Zhevnitskiy <zhenik15@student.westerdals.no>
2017-04-20 13:21:30 +0200

    update application info -> add global variable for % of loan

--- app/src/main/java/woact/android/zhenik/pj/utils/ApplicationInfo.java
+++ app/src/main/java/woact/android/zhenik/pj/utils/ApplicationInfo.java
@@ -9,4 +9,6 @@ public class ApplicationInfo implements Serializable {
     public static final String USER ="USER";
     public static long USER_IN_SYSTEM_ID;
     public static Long GROUP_CLICKED=null;
+    public static final Double LOAN_PERCENT=0.05;
+
 }
